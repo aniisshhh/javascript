29f6f110807442342a64c9a9785f6cedf8fe5a13
aniisshhh <523me0001@iiitk.ac.in>
2025-04-28 07:49:59 +0000

    dataTypes

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -1,5 +1,5 @@
 //Premitive
-// 7 types = string, fnumber, boolean, null, undefined, symbol, BigInt
+// 7 types = string, number, boolean, null, undefined, symbol, BigInt
 const score = 100;
 const scoreValue = 100.3;
 const isLogedIn = false;
@@ -7,7 +7,13 @@ const outsideTemp = null;
 let userEmail; //undefined
 const id = Symbol('123')
 const anotherID = Symbol('123')
-console.log(id === anotherID);
-const bigNumber = 849859853439
+//console.log(id === anotherID);
+//const bigNumber = 849859853439n
+
+
 //reference(Non premitive)
 //Array, object, Function
+const myFunction =function(){
+    console.log("hello world")
+}
+console.log(typeof bigNumber)
