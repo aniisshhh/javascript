52946cf8100163dd82db67911150d19910555321
aniisshhh <523me0001@iiitk.ac.in>
2025-04-30 09:15:26 +0000

    numbers

---
+++ Basics/06_nums_and_math.js
@@ -0,0 +1,17 @@
+const score = 500
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString()); //for conversion of number into string
+console.log(balance.toString().length); // finding the length of thte the string
+console.log(balance.toFixed(2)); // for precision upto 2 digit
+
+const anothernumber = 23.4494
+console.log(anothernumber.toPrecision(4)) // for overall significant digit
+
+const hundred = 100000000
+console.log(hundred.toLocaleString('en-IN'));
+
+//++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
