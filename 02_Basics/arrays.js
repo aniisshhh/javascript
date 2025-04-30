1336c2891ca2527390bc436b5d8202400b4dfb43
aniisshhh <523me0001@iiitk.ac.in>
2025-04-30 15:38:50 +0000

    array

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,15 @@
+const myArray = [0, 10, 2, 3, 4, 5]
+const myHeros = [ "anish", "shaktiman"]
+const myarray2 = new Array(1, 2, 3, 4)
+
+console.log(myArray[1])
+console.log(myHeros[1])
+console.log(myarray2[1])
+
+
+//Arrays method
+myArray.push(55)
+myArray.pop()
+
+
+console.log(myArray)
