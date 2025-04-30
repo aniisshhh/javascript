31eee9a73ce54ad5f9de5dd18dd30d22c82e3a7d
aniisshhh <523me0001@iiitk.ac.in>
2025-04-30 10:04:02 +0000

    maths

--- Basics/06_nums_and_math.js
+++ Basics/06_nums_and_math.js
@@ -14,4 +14,14 @@ console.log(anothernumber.toPrecision(4)) // for overall significant digit
 const hundred = 100000000
 console.log(hundred.toLocaleString('en-IN'));
 
-//++++++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//++++++++++++++++++++++++++++++++Maths+++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-5)); //
+console.log(Math.round(4.501));
+console.log(Math.ceil(4.001));
+console.log(Math.floor(4.9999));
+console.log(Math.min(12,33,44,58,57));
+console.log(Math.max(22,33,44,55,66,77));
+console.log(Math.random()); // any random value between 0 and 1
+console.log(Math.random()*10 +1);
