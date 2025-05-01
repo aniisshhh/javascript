1037b42dec5fbe5d932ff83c07dcd517016e6bba
aniisshhh <523me0001@iiitk.ac.in>
2025-05-01 09:54:38 +0000

    array1

--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -10,6 +10,23 @@ console.log(myarray2[1])
 //Arrays method
 myArray.push(55)
 myArray.pop()
+myArray.unshift(9)
+myArray.shift()
+console.log(myArray.includes(9));
+console.log(myArray.indexOf(9));
+const newArr = myArray.join();
 
+console.log(myArray);
+console.log(newArr);
 
-console.log(myArray)
+// slice, splice
+console.log("A ",myArray);
+const myn1 = myArray.slice(1,3)
+
+console.log(myn1);
+
+console.log("B ",myArray);
+const myn2 = myArray.splice(1,3)
+
+console.log(myn2);
+
