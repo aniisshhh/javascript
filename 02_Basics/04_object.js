1e45408d24b1e1ffc68fd12c37b08d85421770b4
aniisshhh <523me0001@iiitk.ac.in>
2025-05-04 14:56:58 +0000

    object

--- 02_Basics/04_object.js
+++ 02_Basics/04_object.js
@@ -16,5 +16,28 @@ const regularUser ={
         }
     }
 }
-console.log(regularUser.email)
-console.log(regularUser.fullname)
+// console.log(regularUser.email)
+// console.log(regularUser.fullname)
+
+const obj1 = { 1 : "a", 2 : "b"}
+const obj2 = { 3 : "c", 4 : "d"}
+
+//const obj3 = {obj1 , obj2}
+// const obj3 = Object.assign({},obj1 , obj2)
+const obj3 = {...obj1 , ...obj2};
+console.log(obj3);
+
+//++++++++++++++++++++++++++++++
+
+const user = [
+    {
+        gmail : "anish@gmail.com",
+        id : 1,
+
+    },
+    {
+        gmail : "anish@gmail.com",
+        id : 1,
+
+    }
+]
